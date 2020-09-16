f29eda205da6b6e7f9e5edf806feece725cbf67a
Tom <thomas.spencer@codurance.com>
2020-09-16 14:59:11 +0100

    Initial commit

---
+++ src/main/java/com/example/mywalkingskeleton/MyWalkingSkeletonApplication.java
@@ -0,0 +1,13 @@
+package com.example.mywalkingskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWalkingSkeletonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyWalkingSkeletonApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/mywalkingskeleton/MyWalkingSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mywalkingskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWalkingSkeletonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
